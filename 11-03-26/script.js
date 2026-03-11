a6c85ba6bd3f90c3b9d3743b6fa89bcbbb42a3d4
CHALLA SREEDHAR REDDY <challasreedharreddy1549@gmail.com>
2026-03-11 21:54:14 +0530

    Start of JS

---
+++ 11-03-26/script.js
@@ -0,0 +1,53 @@
+// var username = "DRF"
+// if (true) {
+//     // let username = "Sreedhar"
+//     username = "Sreedhar"
+//     console.log("inside " + username)
+// }
+// console.log("Outiside " + username)
+// function hello() {
+//     var name = "Virat"
+//     console.log(name)
+// }
+// hello()
+// var last = "challa"
+// last = 7
+// console.log(last)
+// var a = "5"
+// var b = 2
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a * b)
+// console.log(a + b - 2)
+// console.log(a + b * 2)
+// var a = "bs"
+// var b = 5
+// console.log(a / b)
+// console.log(a = b)
+// console.log(a == b)
+// console.log(a == b)
+// console.log(a < b)
+// console.log(a > b)
+// console.log(a <= b)
+// console.log(a >= b)
+// console.log(a != b)
+// console.log(a !== b)
+// var a = "bs"
+// var b = 5
+// // var c = a && b
+// // var c = a || b
+// // var c = a < b || a < c
+// // var c = a ||
+// console.log(c)
+// console.log(typeof NaN)
+// a = 5
+// d = a++ + ++a + a
+// console.log(d)
+// console.log(d + a)
+a = 5
+b = 66
+c = 888
+d = a > b ? (a > c ? a : c) : (b > c ? b : c)
+console.log(d)
