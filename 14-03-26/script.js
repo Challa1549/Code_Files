d14d6166d616797f9b5bf7ac0d94d659c55dca8a
CHALLA SREEDHAR REDDY <challasreedharreddy1549@gmail.com>
2026-03-14 23:14:05 +0530

    JS Day - 04

---
+++ 14-03-26/script.js
@@ -0,0 +1,56 @@
+// console.log(sum("Hello ", "Sreedhar"));
+// function sum(a, b) {
+//     return a + b
+// }
+// var sum = function (a, b) {
+//     return a + b
+// }
+// console.log("Hello ", "Sreedhar")
+// var sum = () => "Welcome to JS"
+// console.log(sum())
+// var sum = () => {
+//     return "Welcome"
+// }
+// console.log(sum())
+// var sum = (a, b) => a + b
+// console.log(sum(5, 7))
+// setInterval(function () {
+//     console.log("Hello World")
+// }, 2000)
+// (function () {
+//     console.log("hello")
+// })()
+// var arr = [5, 7, 12, 30, 46, 42]
+// arr.forEach(multiplyWith2)
+// function multiplyWith2(a) {
+//     console.log(a * 2)
+// }
+// arr.forEach((a) => {
+//     console.log(a * 2)
+// })
+// arr.forEach(function (a) {
+//     console.log(a * 2)
+// })
+// arr.forEach((a, ind) => {
+//     arr[ind] = a * 2
+// })
+// console.log(arr)
+// var newarr = arr.map((a) => a * 2)
+// console.log(newarr)
+// var newarr = arr.map(((a) => {
+//     if (a % 2 == 0)
+//         return a
+// }))
+// console.log(newarr)
+// var newarr = arr.filter((a) => a % 2 == 0)
+// console.log(newarr)
+// var ele = arr.findIndex[(i) => i.length < 1]
+// var ele = arr.find[(i) => i.length < 1]
+// console.log(ele)
+// var sum = arr.reduce((acc, i) => acc + i)
+// var sum = arr.reduce((acc, i) => acc + i, 10)
+// var sum = arr.filter((i) => i % 2 == 0).reduce((bs, i) => bs + i)
+// console.log(sum)
+// var ispos = arr.every(i => i > 0)
+// var ispos = arr.some(i => i > 0)
+// console.log(ispos)
